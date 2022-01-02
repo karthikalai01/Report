a052b29fba894f196c7ecb0cdf7bb6769b1e5360
karthikalai01 <mechkarthi241094@gmail.com>
2022-01-02 06:42:49 -0800

    Pushcode

---
+++ FirstProject/src/com/java/Methods.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class Methods {
+public static void first(int a,int b) {
+a=18;
+	System.out.println(a);
+	System.out.println(b);
+}
+public static void main(String[] args) {
+	first(7, 67);
+}
+}
